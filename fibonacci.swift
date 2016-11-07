672a1385489384232248f45f3a0551eed8a1521a
Stephanholly <sholly@usc.edu>
2016-11-06 21:57:06 -0700

    first commit

---
+++ fibonacci.swift
@@ -0,0 +1,5 @@
+// The Fibonacci Sequence is a sequence of numbers where the next number is the sum of the two preceding numbers. For example, the Fibonacci Sequence starts: 1,1,2,3,5,8,13,21,34,55,89,144....etc.
+
+// Create a function that takes in a number as an argument and outputs that many numbers of the Fibonacci Sequence. For example, if the argument is 8, the function will return 1,1,2,3,5,8,13,21.
+
+//Like every challenge, there are multiple ways to achieve this. Get to it!
